ab62e22ed5361f07f17d1e9c963cf68ee06604fb
Benjamin Chang <benjaminchang23@gmail.com>
2021-08-02 21:34:58 -0400

    feat: add better file name sanitization

--- src/FileOperations.cc
+++ src/FileOperations.cc
@@ -130,11 +130,16 @@ bool MakeDirectories(const std::string &target_path)
 std::string SanitizeFileName(const std::string &file_name)
 {
     std::string sanatized_file_name = file_name;
-    const std::string unallowed = " /\\*?<>:;=[]!@|";
+    const std::string unallowed = " /\\*?<>:;=[]!@|.";
 
-    if (sanatized_file_name.front() == '-')
+    while (sanatized_file_name.front() == '-')
     {
         sanatized_file_name.erase(sanatized_file_name.begin());
+    }
+
+    while (sanatized_file_name.back() == '-' && sanatized_file_name.size() > 0)
+    {
+        sanatized_file_name.pop_back();
     }
 
     std::transform(sanatized_file_name.begin(), sanatized_file_name.end(), sanatized_file_name.begin(),
@@ -142,6 +147,16 @@ std::string SanitizeFileName(const std::string &file_name)
     {
         return (std::find(unallowed.begin(), unallowed.end(), ch) != unallowed.end()) ? '-' : ch;
     });
+
+    const std::string dup_string = "--";
+
+    size_t pos = sanatized_file_name.find(dup_string);
+
+    while (pos != std::string::npos)
+    {
+        sanatized_file_name.replace(pos, dup_string.size(), "-");
+        pos = sanatized_file_name.find(dup_string);
+    }
 
     return sanatized_file_name;
 }
