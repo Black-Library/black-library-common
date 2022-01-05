e1547d2f94efd5bca25a401bcb0877197b4e4b87
Benjamin Chang <benjaminchang23@gmail.com>
2022-01-05 18:37:54 -0500

    feat: add config operations as header for njson

---
+++ src/ConfigOperations.cc
@@ -0,0 +1,15 @@
+/**
+ * ConfigOperations.cc
+ */
+
+#include <ConfigOperations.h>
+
+namespace black_library {
+
+namespace core {
+
+namespace common {
+
+} // namespace common
+} // namespace core
+} // namespace black_library
