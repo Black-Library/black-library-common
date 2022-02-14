d65ad7677ef8c9646b59fbb3257aab002efcddf4
Benjamin Chang <benjaminchang23@gmail.com>
2022-02-14 02:17:50 -0500

    feat: add some support for time test

--- test/time_test.cc
+++ test/time_test.cc
@@ -15,7 +15,9 @@ namespace common {
 TEST_CASE( "GetGUITimeString generic tests (pass)", "[single-file]" )
 {
     std::time_t test_time_0 = ParseTimet("01/02/2003 7:08:09 PM", "%m/%d/%Y %I:%M:%S %p");
-    REQUIRE( GetGUITimeString(test_time_0) == "" );
+    // 2003-01-02 19:08 EST to GMT is 2003-01-03 00:08
+    std::string gui_str = GetGUITimeString(test_time_0);
+    REQUIRE( gui_str == std::string("2003-01-03 00:08") );
 }
 
 // TEST_CASE( "GetISOTimeString generic tests (pass)", "[single-file]" )
@@ -28,12 +30,14 @@ TEST_CASE( "GetUnixTime generic tests (pass)", "[single-file]" )
     REQUIRE( GetUnixTime() );
 }
 
-// TEST_CASE( "ParseTimet generic tests (pass)", "[single-file]" )
-// {
-//     time_t test_time_0 = GetUnixTime();
+TEST_CASE( "ParseTimet generic tests (pass)", "[single-file]" )
+{
+    time_t test_time_0 = 1540762483 + 5 * 60 * 60; // 5 hour modifier for EST -> GMT
+
+    time_t parse_time_rr = ParseTimet("10/28/2018 9:34:43 PM +00:00", "%m/%d/%Y %I:%M:%S %p");
     
-//     REQUIRE( ParseTimet("", "%m/%d/%Y %I:%M:%S %p") == "" );
-// }
+    REQUIRE( parse_time_rr == test_time_0 );
+}
 
 
 } // namespace common
